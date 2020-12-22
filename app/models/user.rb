6818b48634ceb7452014d0f71d3de6525c25fc91
github username <jhernandez1692@gmail.com>
2020-12-21 21:31:40 -0500

    Created a User model.

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+
+    has_many :competitions
+    has_many :created_workouts, foreign_key: "user_id", class_name: "Workout"
+    has_many :completed_workouts, through: :competitions, source: :workouts
+    
+    
+    has_secure_password
+    validates :username, presence: true
+    validates :username, uniqueness: true
+
+end
