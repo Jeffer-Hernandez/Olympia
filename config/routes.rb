fb046cda84300202554496d67dee23732ab04cda
github username <jhernandez1692@gmail.com>
2020-12-21 21:35:10 -0500

    Crt custom routes for the login, logout, signup.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+
+  root 'sessions#index'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  delete '/signup' => 'users#create'
+
+
+  resources :workouts
+  resources :sessions
+  resources :competitions
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
