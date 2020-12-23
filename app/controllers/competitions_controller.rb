2a9bbe2401a4c8af859bd42f16db5be96047e589
github username <jhernandez1692@gmail.com>
2020-12-23 12:06:20 -0500

    Created a competition controller.

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -1,2 +1,32 @@
 class CompetitionsController < ApplicationController
+
+    def new
+        @competition = Competition.new
+    end
+
+    def create
+        @competition = current_user.competitions.build(competition_params)
+
+        if @competition.save
+            redirect_to competition_path(@competition)
+        else
+            render :new
+        end
+
+    end
+
+    def index
+        @competition = Competition.all
+    end
+
+    def show
+        @competition = Competition.find_by(id: params[:id])
+    end
+
+    private
+
+    def competition_params
+        params.require(:competition).permit(:title, :category, :description)
+    end
+
 end
