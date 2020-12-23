bf0c698cb1fc6293589f4a884a30971c852a2994
github username <jhernandez1692@gmail.com>
2020-12-23 12:07:20 -0500

    Created a workouts controller.

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,35 @@
 class WorkoutsController < ApplicationController
+
+    def new
+        @workout = Workout.new
+    end
+
+    def create
+        @workout = current_user.created_workouts.build(workout_params)
+
+        if @workout.save
+            redirect_to workout_path(@workout)
+        else
+            render :new
+        end
+        
+    end
+
+    def index
+        @workouts = Workout.all
+    end
+
+
+    def show
+        @workout = Workout.find_by(id: params[:id])
+    end
+
+
+    private
+
+    def workout_params
+        params.require(:workout).permit(:category, :intensity, :user_id)
+    end
+
+
 end
