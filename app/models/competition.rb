749331e7c049ab1f80ec60304e7f4ebbcf6d4991
github username <jhernandez1692@gmail.com>
2020-12-24 12:14:10 -0500

    Validations for Competition model.

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,6 +1,9 @@
 class Competition < ApplicationRecord
   belongs_to :user
-  has_many :sessions
-  has_many :workouts, through: :sessions
-  
+  has_many :training_sessions
+  has_many :workouts, through: :training_sessions
+
+  validates :title, :description, :category, presence: true
+
+
 end
