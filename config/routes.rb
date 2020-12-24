2e77c444d637e7a7c8580445875c43df7ba105d2
github username <jhernandez1692@gmail.com>
2020-12-24 12:15:23 -0500

    Add nested routes & restructureed.

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,28 @@ Rails.application.routes.draw do
 
   root 'sessions#index'
 
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
 
-  get '/signup' => 'users#new'
-  post '/signup' => 'users#create'
+  get '/competitions/:id/edit' => 'competitions#edit'
+  post '/competitions/:id/edit' => 'competitions#update'
 
 
-  resources :workouts
+
+  resources :workouts do
+    resources :training_sessions, only: [:index, :new, :create]
+  end
+
+  resources :competitions do
+    resources :training_sessions
+  end
+  
   resources :training_sessions
-  resources :competitions
+
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
