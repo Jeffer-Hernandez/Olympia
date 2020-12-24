809f6b17f68822680cae145187076ed2d051c73a
github username <jhernandez1692@gmail.com>
2020-12-24 12:07:25 -0500

    Include validations for presence of Workout attrs.

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,4 +2,10 @@ class Workout < ApplicationRecord
   belongs_to :user
   has_many :training_sessions
   has_many :competitions, through: :training_sessions
+
+  validates :name, :category, :intensity, presence: true
+
+  
+
+  
 end
