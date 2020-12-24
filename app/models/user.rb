267090b1b04b2bf53ebb615affab9127cc33b8b3
github username <jhernandez1692@gmail.com>
2020-12-24 12:31:19 -0500

    has many to has many through relationship.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,13 @@ class User < ApplicationRecord
     has_many :competitions
     has_many :created_workouts, foreign_key: "user_id", class_name: "Workout"
     has_many :completed_workouts, through: :competitions, source: :workouts
-    has_many :training_sessions
+    has_many :training_sessions, through: :competitions
     
     
     has_secure_password
     validates :username, presence: true
     validates :username, uniqueness: true
+
+
 
 end
