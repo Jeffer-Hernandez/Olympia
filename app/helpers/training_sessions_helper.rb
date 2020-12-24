4d1f6550357529743d0082be7a01301ee05ec7ec
github username <jhernandez1692@gmail.com>
2020-12-24 12:34:15 -0500

    display index header helper for TS

--- app/helpers/training_sessions_helper.rb
+++ app/helpers/training_sessions_helper.rb
@@ -1,4 +1,12 @@
 module TrainingSessionsHelper
-
+    def display_index_header
+        if @workout
+         content_tag(:h2,"Training Sessions for the  #{@workout.name} workout")
+        elsif @competition
+          tag.h2("Training Sessions for the #{@competition.title} competition")
+        else
+         content_tag(:h1, "All Training Sessions")
+        end
+    end
 
 end
