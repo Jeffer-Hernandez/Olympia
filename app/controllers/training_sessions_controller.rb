c68d3a8b2c19abc25affe4c8b2aec1910a189e92
github username <jhernandez1692@gmail.com>
2020-12-24 12:10:02 -0500

    Restructured TS controller.

--- app/controllers/training_sessions_controller.rb
+++ app/controllers/training_sessions_controller.rb
@@ -1,48 +1,56 @@
 class TrainingSessionsController < ApplicationController
 
-    def new
-        @training_session = TrainingSession.new
-    end
-
-
-    # def new
-
-    #     # if params[:workout_id] && @workout = Workout.find_by_id(params[:workout_id])
-    #     #     @training_session = @workout.training_sessions.build #has_many
-    #     # else
-    #     #     @training_session = TrainingSession.new
-    #     #     @training_session.build_workout
-    #     # end
-
-    #     @training_session = TrainingSession.new(training_session_params)
-
-
-
-    # end
-
-    def create
-        @training_session = TrainingSession.new(training_session_params)
-
-        if @training_session.save
-            redirect_to training_session_path(@training_session)
-        else
-            render :new
-        end
-
-    end
-
-    def index
-        @training_sessions = TrainingSession.find_by(user_id: params[:user_id])
-    end
-
     def show
-        @training_session = TrainingSession.find_by(id: params[:id])
+        @training_session = TrainingSession.find(params[:id])
     end
+    
+    def index
 
-    # private
+        if params[:workout_id] && @workout = Workout.find_by_id(params[:workout_id])
+          @training_sessions = @workout.training_sessions
 
-    def training_session_params
-        params.require(:training_session).permit(:date, :condition, :competition_id, :workout_id, :user_id)  
+        elsif params[:competition_id] && @competition = Competition.find_by_id(params[:competition_id])
+          @training_sessions = @competition.training_sessions
+        else
+
+          @training_sessions = TrainingSession.all
+        end
     end
+    
+    def new
+        if params[:workout_id] && @workout = Workout.find_by_id(params[:workout_id])
+          @training_session = @workout.training_sessions.build #has_many
+        else
+          @training_session = TrainingSession.new
+        end
+    end
+    
+    def create
+        
+        if params[:workout_id] && @workout = Workout.find_by_id(params[:workout_id])
+          @training_session = @workout.training_sessions.build(training_session_params)
+        else
+           
+          @training_session = TrainingSession.new(training_session_params)
+        end
+        
+        if @training_session.save
+            
+          redirect_to training_session_path(@training_session)
+        else
+            
+          render :new
+        end
+    end
+    
+    private
+    
+        def training_session_params
+
+            params.require(:training_session).permit(:date, :condition, :competition_id, :workout_id)
+            
+        end
+    
 
 end
+
