c48157c9f62e4ecca9959b20b216012c137cb8dc
github username <jhernandez1692@gmail.com>
2020-12-24 12:13:11 -0500

    set_workout method.

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,35 +1,35 @@
 class WorkoutsController < ApplicationController
 
-    def new
-        @workout = Workout.new
-    end
-
-    def create
-        @workout = current_user.created_workouts.build(workout_params)
-
-        if @workout.save
-            redirect_to workout_path(@workout)
-        else
-            render :new
-        end
-        
-    end
+    before_action :set_workout, only: [:show]
 
     def index
-        @workouts = Workout.all
+      @workouts = Workout.all
     end
-
-
+  
     def show
-        @workout = Workout.find_by(id: params[:id])
     end
-
-
+  
+    def new
+      @workout = Workout.new
+    end
+  
+    def create
+      @workout = current_user.created_workouts.build(workout_params)
+      if @workout.save
+        redirect_to workout_path(@workout)
+      else
+        render :new
+      end
+    end
+  
     private
-
-    def workout_params
-        params.require(:workout).permit(:category, :intensity, :user_id)
-    end
-
-
+  
+     def workout_params
+        params.require(:workout).permit(:intensity, :name, :category)
+     end
+  
+     def set_workout
+        @workout = Workout.find_by(id: params[:id])
+        redirect_to workouts_path if !@workout
+     end
 end
