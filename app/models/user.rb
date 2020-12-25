55cc74d8ef0d17a3bbabe46c9aaa11954ff5ed8f
github username <jhernandez1692@gmail.com>
2020-12-24 19:44:46 -0500

    include email attr & omniauth method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,17 @@ class User < ApplicationRecord
     
     
     has_secure_password
-    validates :username, presence: true
-    validates :username, uniqueness: true
+    validates :username, :email, presence: true
+    validates :username, :email, uniqueness: true
+
+    def self.from_omniauth(auth)
+        where(email: auth.info.email).first_or_initialize do |user|
+          user.username = auth.info.name
+          user.email = auth.info.email
+          user.password = SecureRandom.hex
+        end
+    end
+
+
 
 end
