9cd916d1f8ec9e4fed817e8f10a37ba2322a6edf
github username <jhernandez1692@gmail.com>
2020-12-24 19:42:55 -0500

    remove rendundant attrs from user model.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
     private
 
         def user_params
-            params.require(:user).permit(:username, :password, :name, :age, :height, :weight)
+            params.require(:user).permit(:username, :password, :email)
         end
 
 end
